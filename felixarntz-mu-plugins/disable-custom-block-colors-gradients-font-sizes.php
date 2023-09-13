64ac3637b74fabf4281e845ab352586d711c1379
Felix Arntz <flixos90@gmail.com>
2023-09-13 18:23:31 -0500

    Introduce module to disable custom block editor features.

---
+++ felixarntz-mu-plugins/disable-custom-block-colors-gradients-font-sizes.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Plugin Name: Disable Custom Block Editor Features
+ * Plugin URI: https://github.com/felixarntz/felixarntz-mu-plugins
+ * Description: Disables custom colors, custom gradients, custom font sizes etc. for the block editor to enforce a uniform style.
+ * Author: Felix Arntz
+ * Author URI: https://felix-arntz.me
+ * License: GPLv2 or later
+ * License URI: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * Text Domain: felixarntz-mu-plugins
+ *
+ * @package felixarntz-mu-plugins
+ */
+
+namespace Felix_Arntz\MU_Plugins;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+add_filter(
+	'block_editor_settings_all',
+	static function ( $editor_settings ) {
+		$editor_settings['disableCustomColors']       = true;
+		$editor_settings['disableCustomGradients']    = true;
+		$editor_settings['disableCustomFontSizes']    = true;
+		$editor_settings['disableCustomSpacingSizes'] = true;
+
+		return $editor_settings;
+	}
+);
